d6b6381514177c98f56048438c1cc5b77629996b
Michael Yang <myang1@wayfair.com>
2019-12-29 00:44:26 -0500

    adding functionality to change num of adults

--- scripts.js
+++ scripts.js
@@ -13,13 +13,25 @@ const dateOptions = {
 const initialTime = new Date();
 const timeElement = document.getElementById('time');
 const dateElement = document.getElementById('date');
-const expiresElement = document.getElementById('expires');
+const expireTimeElement = document.getElementById('expireTime');
+const numAdultElement = document.getElementById('numAdult');
 
 function updateTime() {
   const now = new Date();
   timeElement.innerHTML = now.toLocaleString('en-US', timeOptions);
   dateElement.innerHTML = now.toLocaleString('en-US', dateOptions);
+  expireTimeElement.innerHTML = '00:00:' + msConversion(3599999 - (now - initialTime));
   setTimeout(updateTime, 1000);
+}
+
+function msConversion(millis) {
+  let sec = Math.floor(millis / 1000);
+  let min = Math.floor(sec / 60);
+  sec -= min * 60;
+
+  sec = '' + sec;
+  sec = ('00' + sec).substring(sec.length);
+  return min + ':' + sec;
 }
 
 function getRandomColor() {
@@ -35,4 +47,9 @@ updateTime();
 document.getElementById('barcode1').style.backgroundColor = getRandomColor();
 document.getElementById('barcode2').style.backgroundColor = getRandomColor();
 document.getElementById('barcode3').style.backgroundColor = getRandomColor();
-document.getElementById('expires').addEventListener('back', () => {});
+document.getElementById('barcode1').addEventListener('click', () => {
+  numAdultElement.innerText = Math.max(parseInt(numAdultElement.innerText) - 1, 1);
+});
+document.getElementById('barcode3').addEventListener('click', () => {
+  numAdultElement.innerText = parseInt(numAdultElement.innerText) + 1;
+});
